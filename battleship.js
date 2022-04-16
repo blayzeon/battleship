43fa246d6edaf79ab90a073832e7c5120ba52766
blayzeon <blayzeon@gmail.com>
2022-04-16 17:11:05 -0400

    add ability to damage ships accurately with gameboard

--- battleship.js
+++ battleship.js
@@ -1,10 +1,32 @@
-const ship = function createBattleship (length=1, sunk=false) {
+export { ship, gameboard };
+
+const setOrientation = function generateCoordsAndDamageObj(start, length, position){
+    let newCoords = [];
+
+    let h = 0;
+    let v = 0;
+
+    for (let i = 0; i < length; i += 1){
+        newCoords.push({coords: [start[0] + v, start[1] + h], damaged: false});
+
+        if (position === "h"){
+            h += 1;
+        } else {
+            v += 1;
+        }
+    }
+
+    return newCoords;
+}
+
+const ship = function createBattleship (coords=[1, 1], length=1, position='h', sunk=false) {
     return {
         length,
-        damage: Array(length).fill(false),
+        damage: setOrientation(coords, length, position),
+        position,
         sunk,
         hit: function (index) {
-            this.damage[index] = true;
+            this.damage[index].damaged = true;
         },
         isSunk: function () {
             for (let i = 1; i < this.damage.length; i += 1 ){
@@ -25,22 +47,37 @@ const gameboard = function createPlayerGameboard () {
             misses: []
         },
         placeship: function (coords, length) {
-            const newShip = ship(length);
-            this.board.placed.push({
-                ship: newShip,
-                coords,
-            });
+            const newShip = ship(coords, length);
+            this.board.placed.push(newShip);
             
         },
         recieveAttack: function (coords) {
-            const target = this.board.placed.filter(place => place === coords);
-            if (target){
-                target.hit(coords);
-            } else {
+            const ships = this.board.placed;
+            function checkIndexes(){
+                
+                let result = false;
+                ships.forEach(x => x.damage.forEach((y)=> {
+                    if (y.coords.toString() === coords.toString()){
+                        result = [ships.indexOf(x), x.damage.indexOf(y)];
+                    }
+                }))
+
+                return result;
+            };
+
+            const indexes = checkIndexes();
+            if (0 === false){
                 this.board.misses.push(coords);
+            } else {
+                this.board.placed[indexes[0]].hit(indexes[1]);
             }
         }
     }
 }
 
-export { ship, gameboard };
+const gb = gameboard();
+gb.placeship([1, 1], 2);
+gb.recieveAttack([1, 2]);
+console.log(gb.board.placed[0].damage[1].damaged)
+
+// ships.forEach(x => x.damage.forEach(y => console.log(ships.indexOf(x) + ' ' + x.damage.indexOf(y))));
--- battleship.test.js
+++ battleship.test.js
@@ -1,29 +1,16 @@
-const { test, expect } = require('@jest/globals');
 import { ship, gameboard } from './battleship.js';
+const { test, expect } = require('@jest/globals');
 
 test('properly applies a length to a new ship', () => {
-    const newShip = ship(2);
+    const newShip = ship([1, 1,], 2);
 
     expect(newShip.length).toBe(2);
-});
-
-test('new ships aren\'t damaged', () => {
-    const newShip = ship();
-
-    expect(newShip.damage).toStrictEqual([ false ]);
 });
 
 test('new ships aren\'t sunk', () => {
     const newShip = ship();
 
     expect(newShip.sunk).toBe(false);
-});
-
-test('damaged ships are damaged', () => {
-    const newShip = ship(2);
-    newShip.hit(1)
-
-    expect(newShip.damage).toStrictEqual([ false, true ]);
 });
 
 test('completely damaged ships are sunk', () => {
@@ -46,5 +33,13 @@ test('gameboard is able to damage ships', () => {
     board.placeship([1, 1], 1);
     board.recieveAttack([1, 1]);
 
-    expect(board.board.placed[0].damage).toStrictEqual([true]);
+    expect(board.board.placed[0].damage[0].damaged).toStrictEqual(true);
+});
+
+test('gameboard is able to precisely damage ships', () => {
+    const board = gameboard();
+    board.placeship([1, 1], 2);
+    board.recieveAttack([1, 2]);
+
+    expect(board.board.placed[0].damage[1].damaged).toStrictEqual(true);
 });
